a9b8da5b8653711ea805799ff20a6dbdc9a65cdd
Uesley S. Silva <uesley@pandeco.com.br>
2018-06-10 17:31:21 -0300

    Realizada a configuração do app.php para produção

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Pandeco API'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://api.pandeco.com.br'),
 
     /*
     |--------------------------------------------------------------------------
