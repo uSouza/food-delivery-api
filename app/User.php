95f7dea12a4e9412d40b8a45922c26f2ca0ebd4c
Uesley S. Silva <uesley@pandeco.com.br>
2018-07-05 19:48:06 -0300

    teste commit php storm

--- app/User.php
+++ app/User.php
@@ -10,7 +10,7 @@ class User extends Authenticatable
 {
     use Notifiable, HasApiTokens;
     /**
-     * The attributes that are mass assignable.
+     * The attributes that are mass assignable.teste
      *
      * @var array
      */
