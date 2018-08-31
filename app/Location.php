6af0300c629b58cf4d159795ad864a30f0e857b0
Uesley Souza <uesleysouza97@gmail.com>
2018-08-30 23:20:20 -0300

    Adicionado o observation ao fillable de Locations

--- app/Location.php
+++ app/Location.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Location extends Model
 {
     protected $fillable = [
-        'city', 'state', 'address', 'number', 'district',
+        'city', 'state', 'address', 'number', 'district', 'observation',
         'postal_code', 'latitude', 'longitude', 'client_id', 'company_id'
     ];
 
