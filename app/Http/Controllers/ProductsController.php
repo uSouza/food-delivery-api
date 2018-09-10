685f98be348d61c98284af392ec27e95042fcf63
Uesley Souza <uesleysouza97@gmail.com>
2018-09-10 20:42:26 -0300

    Realizada no retorno do produto na inserção.

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -31,7 +31,7 @@ class ProductsController extends Controller
 
         $product->ingredients()->attach($ingredients_ids);
 
-        return Product::where('id', $product->id)->with(['price', 'ingredients', 'additionals'])->get();
+        return Product::where('id', $product->id)->with(['price', 'ingredients'])->get();
     }
 
     public function show($id)
@@ -60,7 +60,7 @@ class ProductsController extends Controller
             }
         }
         $product->ingredients()->attach($ingredients_ids);
-        return Product::where('id', $product->id)->with(['price', 'ingredients', 'additionals'])->get();
+        return Product::where('id', $product->id)->with(['price', 'ingredients'])->get();
     }
 
     public function destroy(Product $product)
