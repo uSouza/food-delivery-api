fd22d9d20daf5cd859b2acf0c049d8bc8e5e49db
Uesley Souza <uesleysouza97@gmail.com>
2018-10-10 19:00:56 -0300

    Correção nomemclatura de rota

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::middleware('auth:api')->prefix('v1')->group(function () {
     Route::get('/ingredient_groups/menu/{id}', 'IngredientGroupsController@ingredientsByMenu');
     Route::get('/additionals/restore/{id}', 'AdditionalsController@restore');
     Route::get('/menus/restore/{id}', 'MenusController@restore');
-    Route::get('company/available', 'CompaniesController@getAvailableCompanies');
+    Route::get('companies/available', 'CompaniesController@getAvailableCompanies');
     Route::resources([
         'clients' => 'ClientsController',
         'users' => 'UsersController',
