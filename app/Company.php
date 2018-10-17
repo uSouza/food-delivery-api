9f5d854e29aafa9a169e8d4460ddc74286856a3e
Uesley Souza <uesleysouza97@gmail.com>
2018-10-16 22:44:29 -0300

    Realizado ajuste no retorno de restaurantes.

--- app/Company.php
+++ app/Company.php
@@ -54,7 +54,7 @@ class Company extends Model
 
     public function user()
     {
-        return $this->hasOne(User::class);
+        return $this->belongsTo(User::class);
     }
 
     public function tags()
