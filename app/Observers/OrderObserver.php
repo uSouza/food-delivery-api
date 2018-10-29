1ed33c8cac29a03bbe8d2f0f33283af8cf60da68
Uesley Souza <uesleysouza97@gmail.com>
2018-10-29 19:02:58 -0300

    Criado envio de push de pedido recebido

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -78,4 +78,39 @@ class OrderObserver
         );
 
     }
+
+    public function created(Order $order) {
+        $client = Client::findOrFail($order->client_id);
+        $company = Company::findOrFail($order->company_id);
+        $user = User::findOrFail($company->user_id);
+
+        $content = null;
+        $heading = null;
+
+        $content = array(
+            "en" => 'Novo pedido recebido :)'
+        );
+        $heading = array(
+            "en" => 'Pedido recebido'
+        );
+
+        $client_http = new \GuzzleHttp\Client();
+        $client_http->post(
+            'https://onesignal.com/api/v1/notifications',
+            [
+                \GuzzleHttp\RequestOptions::JSON =>
+                    [
+                        'app_id' => "f9e6f12d-e1d9-449f-b5e8-0294874f286c",
+                        'include_player_ids' => array($user->onesignal_id),
+                        'data' => array("foo" => "bar"),
+                        'contents' => $content,
+                        'headings' => $heading
+                    ]
+            ],
+            [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Basic YWVhOTUyMzItMDc0YS00MTBmLTkxMGYtOTFmZTAwN2MzMTkw'
+            ]
+        );
+    }
 }
