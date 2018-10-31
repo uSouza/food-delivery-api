e108e97dc08b139f0cf69abb913338febdb82001
Uesley Souza <uesleysouza97@gmail.com>
2018-10-31 18:18:17 -0300

    Alterada a coluna de sizes no banco de dados.

---
+++ database/migrations/2018_10_31_180311_alter_size_to_prices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterSizeToPricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('prices', function (Blueprint $table) {
+            $table->dropColumn('size');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('prices', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ database/migrations/2018_10_31_180815_add_size_to_prices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSizeToPricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('prices', function (Blueprint $table) {
+            $table->string('size');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('prices', function (Blueprint $table) {
+            //
+        });
+    }
+}
